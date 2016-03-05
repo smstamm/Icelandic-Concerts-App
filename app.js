e1b31b92cc183cc8a55884d9acdbe8dc635a5680
smstamm <steph.m.stamm@gmail.com>
2016-03-05 13:57:06 -0700

    fixes small class error on ride share

--- app.js
+++ app.js
@@ -1,30 +1,30 @@
 var app = angular.module("concertApp",[]);
 
 app.controller('concertController', ['$scope', 'concerts', 'carpools', function($scope, concerts, carpools) {
-  {concerts.success(function(data) {
-    $scope.concerts = data.results;
-  })};
-  {carpools.success(function(data) {
-    $scope.carpools = data.results;
-  })};
+	{concerts.success(function(data) {
+		$scope.concerts = data.results;
+	})};
+	{carpools.success(function(data) {
+		$scope.carpools = data.results;
+	})};
 }]);
 
 app.factory('concerts', ['$http', function($http) {
-  return $http.get('http://apis.is/concerts')
-      .success(function(data) {
-        return data;
-      })
-      .error(function(err) {
-        return err;
-      });
+	return $http.get('http://apis.is/concerts')
+	.success(function(data) {
+		return data;
+	})
+	.error(function(err) {
+		return err;
+	});
 }]);
 
 app.factory('carpools', ['$http', function($http) {
-  return $http.get('http://apis.is/rides/samferda-drivers/')
-      .success(function(data) {
-        return data;
-      })
-      .error(function(err) {
-        return err;
-      });
+	return $http.get('http://apis.is/rides/samferda-drivers/')
+	.success(function(data) {
+		return data;
+	})
+	.error(function(err) {
+		return err;
+	});
 }]);
